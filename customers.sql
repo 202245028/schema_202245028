34f0ae4be33ff2d55475e27a846cbaf17f640201
202245028 <0b000000@naver.com>
2025-04-13 23:04:47 +0900

    Create customers.sql

---
+++ customers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+  customer_id VARCHAR(10) PRIMARY KEY,
+  company_name VARCHAR(100),
+  contact_name VARCHAR(50),
+  contact_title VARCHAR(50),
+  address VARCHAR(200),
+  city VARCHAR(50),
+  region VARCHAR(50),
+  phone VARCHAR(50),
+  mileage INT
+);
